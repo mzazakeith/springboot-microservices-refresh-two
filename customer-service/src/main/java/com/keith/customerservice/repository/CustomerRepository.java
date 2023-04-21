2c1422026d7f58adfd47401cb296c120c334ebf0
mzazakeith <mzazakeith@gmail.com>
2023-04-21 16:39:53 +0300

    Initial repository

---
+++ customer-service/src/main/java/com/keith/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.keith.customerservice.repository;
+
+import com.keith.customerservice.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+}
--- customer-service/src/main/java/com/keith/customerservice/service/impl/CustomerServiceImpl.java
+++ customer-service/src/main/java/com/keith/customerservice/service/impl/CustomerServiceImpl.java
@@ -1,13 +1,18 @@
 package com.keith.customerservice.service.impl;
 
 import com.keith.customerservice.model.dto.CustomerRequest;
+import com.keith.customerservice.repository.CustomerRepository;
 import com.keith.customerservice.service.CustomerService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
     @Override
     public void registerCustomer(CustomerRequest customerRequest) {
         customerRepository.save(customerRequest);
