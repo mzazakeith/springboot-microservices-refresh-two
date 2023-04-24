3c250dc2fd48950e91dce13c505b2aa0f91fcf0a
mzazakeith <mzazakeith@gmail.com>
2023-04-24 16:22:35 +0300

    Product Request and Product Response

--- product-service/src/main/java/com/keith/productservice/model/dto/request/ProductRequest.java
+++ product-service/src/main/java/com/keith/productservice/model/dto/request/ProductRequest.java
@@ -1,4 +1,23 @@
 package com.keith.productservice.model.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductRequest {
+
+    @JsonProperty("product_name")
+    private String name;
+    @JsonProperty("product_description")
+    private String description;
+    @JsonProperty("product_price")
+    private BigDecimal price;
 }
--- product-service/src/main/java/com/keith/productservice/model/dto/request/ProductResponse.java
+++ product-service/src/main/java/com/keith/productservice/model/dto/request/ProductResponse.java
@@ -1,4 +1,34 @@
 package com.keith.productservice.model.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductResponse {
+
+    @JsonProperty("product_name")
+    private String name;
+
+    @JsonProperty("product_description")
+    private String description;
+
+    @JsonProperty("product_price")
+    private BigDecimal price;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
+    private LocalDateTime updatedAt;
 }
