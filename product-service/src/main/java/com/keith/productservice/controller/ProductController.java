3c7cd324e35adad1403c53f3749700e07b09f46e
mzazakeith <mzazakeith@gmail.com>
2023-04-24 14:19:22 +0300

    Intial prduct request

--- product-service/src/main/java/com/keith/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/keith/productservice/controller/ProductController.java
@@ -1,9 +1,28 @@
 package com.keith.productservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.keith.productservice.model.dto.ProductRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/v1/product")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
 }
